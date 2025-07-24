14217c45ca4f3ccac5fa2498a6a0c4e56466ff42
Shafi031 <shohanurrahman163@outlook.com>
2025-07-24 16:49:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Counter from './counter'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>MY COUNTER</h1>
+      
+      <Counter></Counter>
+    </>
+  )
+}
+
+export default App
---
+++ src/counter.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+
+export default function Counter(){
+    const [count,setCount] =useState(0)
+    
+    
+
+    const handleAdd = ()=>{
+        const newCount=count+1;
+        setCount(newCount)
+    }
+    const handleRemove = ()=>{
+        if(count> 0){
+            const newCount=count-1;
+            setCount(newCount)
+        }
+        else{
+            alert("Minus is not show")
+        }
+    }
+    const handleZero = ()=>{
+        const newCount=0;
+        setCount(newCount)
+    }
+
+    const CardStyle ={
+        margin : '5px'
+    }
+
+    const Card ={
+        border : '2px solid white',
+        padding : '10px',
+        borderRadius: '10px'
+    }
+
+    const countStyle ={
+        border:'5px solid red',
+        padding:'10px',
+        borderRadius:'10px',
+        background:'grey'
+    }
+
+
+    return(
+        <div style={Card}>
+            <h3 style={countStyle}>Count: {count}</h3>
+            <button style={CardStyle} onClick={handleAdd}>ADD</button>
+            <button style={CardStyle} onClick={handleRemove}>REMOVE</button>
+            <button style={CardStyle} onClick={handleZero}>UNDA</button>
+        </div>
+    )
+}
